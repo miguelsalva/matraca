f617fc2459528d067d0efefafadd0a982256912c
miguelsalva <miguelsalva@gmail.com>
2023-03-02 12:26:30 +0100

    First commit

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+
+- [ ] Find an updated list of the top companies in the world and in Spain
+- [ ] Find a way to get the name of every company from that list and move them into an internal structure
+- [ ] Create the Linkedin URL pattern from each of the names of the structure: i.e: https://www.linkedin.com/company/google
+- [ ] Find how to get from the Linkedin company website into the Linkedin company jobs website
+- [ ] Scrap each of the Linkedin company job websites to get the number of vacancies in the world and in Spain
+- [ ] Sort the companies by number of vacancies in the world and in Spain
+- [ ] Show those sorted lists 
+
---
+++ matraca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# FILE: matraca.py
+# AUTHOR: Miguel Salvá
+# LICENSE: MIT License. Copyright (c) 2023 Miguel Salvá
+# ABSTRACT: PoC for getting job vacancies from Linkedin company websites
+#           Not to be run - only for educational purposes
+#
+# This script requires the beautifulsoup4 and requests libraries to run
+
+
+import sys
+import requests 
+from bs4 import BeautifulSoup
+
+# URL = sys.argv[1]
+URL = "https://www.linkedin.com/jobs/meli%C3%A1-hotels-international-jobs-worldwide?f_C=10838&trk=job-results_see-all-jobs-link&position=1&pageNum=0"
+string1 = ""
+
+
+req = requests.get(URL)
+html = BeautifulSoup(req.text, 'html.parser')
+
+#print(html.title)
+#print(len(html.title.contents))
+string1 = str(html.title)
+#print(string1)
+#print(len(string1))
+print(string1[7:10])
+print("")
