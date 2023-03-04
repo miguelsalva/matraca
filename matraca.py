dfbab21080d6edc00b95b58cb89807f84fcb0d93
miguelsalva <miguelsalva@gmail.com>
2023-03-04 19:27:31 +0100

    Commented old code

--- matraca.py
+++ matraca.py
@@ -13,18 +13,19 @@ import sys
 import requests 
 from bs4 import BeautifulSoup
 
+################################################
 # URL = sys.argv[1]
-URL = "https://www.linkedin.com/jobs/meli%C3%A1-hotels-international-jobs-worldwide?f_C=10838&trk=job-results_see-all-jobs-link&position=1&pageNum=0"
-string1 = ""
+#URL = "https://www.linkedin.com/jobs/meli%C3%A1-hotels-international-jobs-worldwide?f_C=10838&trk=job-results_see-all-jobs-link&position=1&pageNum=0"
+#string1 = ""
 
-
-req = requests.get(URL)
-html = BeautifulSoup(req.text, 'html.parser')
+#req = requests.get(URL)
+#html = BeautifulSoup(req.text, 'html.parser')
 
 #print(html.title)
 #print(len(html.title.contents))
-string1 = str(html.title)
+#string1 = str(html.title)
 #print(string1)
 #print(len(string1))
-print(string1[7:10])
-print("")
+#print(string1[7:10])
+#print("")
+###############################################
