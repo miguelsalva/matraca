1d88dc19e38aaa8a56a808097c7089b29b4615bb
miguelsalva <miguelsalva@gmail.com>
2023-03-09 07:24:07 +0100

    Added a function to get name from dataset and align the output along the rank

--- matraca.py
+++ matraca.py
@@ -17,15 +17,28 @@ from bs4 import BeautifulSoup
 DATASET = "https://www.forbes.com/sites/forbesstaff/2022/05/12/forbes-global-2000-list-2022-the-top-200/"
 
 
-def print_position(cadena):
+def print_company_position(cadena):
     """Function that returns the portion of string between the "#" and a blankspace"""
     for i in range(len(cadena)):
-      if cadena[i] == "#":
-          j = i
-          while cadena[j] != " ":
-              j = j+1
-          return cadena[i:j] 
-        
+        if cadena[i] == "#":
+            j = i
+            while cadena[j] != " ":
+                j = j+1
+            return cadena[i:j] 
+
+
+def print_company_name(cadena):
+    """Function that returns the portion of string between quotes after the "|" """
+    for i in range(len(cadena)):
+        if cadena[i] == "|":
+            while cadena[i] != '"':
+                i = i+1
+            else:
+                j = i+1
+                while cadena[j] != '"':
+                  j = j +1
+                return cadena[i+1:j]
+      
 
 # Main
 req = requests.get(DATASET)
@@ -35,8 +48,13 @@ contenido = html.find(class_='article-body')
 contenido_empresas = contenido.find_all('h3')
 
 for componente in contenido_empresas:
+    #print(componente)
     componente = str(componente)
-    print(componente)
-    num = print_position(componente)
-    print(num)
-    print("")
+    company_rank = print_company_position(componente)
+    company_name = print_company_name(componente)
+    print(company_rank, end="")
+    print(": ", end="")
+    print(company_name)
+
+
+
